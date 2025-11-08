cbb04f66f39808ca88d4c4f24d4c94d6300ba8f2
evanpaul <evanbobanpaul@gmail.com>
2025-11-08 10:42:49 -0800

    fix file missing error in main.py

--- main.py
+++ main.py
@@ -266,7 +266,6 @@ if __name__ == '__main__':
     app.run(
         host='0.0.0.0', 
         port=8000, 
-        debug=False,
-        ssl_context=('cert.pem', 'key.pem')  
+        debug=False, 
     )
 
